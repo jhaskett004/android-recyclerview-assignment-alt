77dba864675cca4d9983c3847a2e0c5081eb673f
jhaskett004 <54286831+jhaskett004@users.noreply.github.com>
2020-10-30 11:53:29 -0500

    first commit

--- app/src/main/java/com/ualr/recyclerviewassignment/MainActivity.java
+++ app/src/main/java/com/ualr/recyclerviewassignment/MainActivity.java
@@ -1,14 +1,20 @@
 package com.ualr.recyclerviewassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.ualr.recyclerviewassignment.Utils.DataGenerator;
+import com.ualr.recyclerviewassignment.adapter.AdapterList;
+import com.ualr.recyclerviewassignment.databinding.ActivityListMultiSelectionBinding;
+import com.ualr.recyclerviewassignment.model.Inbox;
 
-// TODO 05. Create a new Adapter class and the corresponding ViewHolder class in a different file. The adapter will be used to populate
-//  the recyclerView and manage the interaction with the items in the list
+import java.util.List;
+
 // TODO 06. Detect click events on the list items. Implement a new method to toggle items' selection in response to click events
 // TODO 07. Detect click events on the thumbnail located on the left of every list row when the corresponding item is selected.
 //  Implement a new method to delete the corresponding item in the list
@@ -17,19 +23,33 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton mFAB;
+    private ActivityListMultiSelectionBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_multi_selection);
+        mBinding = ActivityListMultiSelectionBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
         initComponent();
     }
 
     private void initComponent() {
-        // TODO 01. Generate the item list to be displayed using the DataGenerator class
-        // TODO 03. Do the setup of a new RecyclerView instance to display the item list properly
-        // TODO 04. Define the layout of each item in the list
-        // TODO 09. Create a new instance of the created Adapter class and bind it to the RecyclerView instance created in step 03
+        // Generated the item list to be displayed using the DataGenerator class
+        List<Inbox> items = DataGenerator.getInboxData(this);
+        items.addAll(DataGenerator.getInboxData(this));
+
+        // Did the setup of a new RecyclerView instance to display the item list properly
+        // Define the layout of each item in the list
+        RecyclerView inboxListView = mBinding.recyclerView;
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        inboxListView.setLayoutManager(layoutManager);
+
+
+        // Created a new instance of the created Adapter class and bind it to the RecyclerView instance created in step 03
+        AdapterList adapter = new AdapterList(items);
+        inboxListView.setAdapter(adapter);
+
         mFAB = findViewById(R.id.fab);
         mFAB.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/ualr/recyclerviewassignment/adapter/AdapterList.java
@@ -0,0 +1,73 @@
+package com.ualr.recyclerviewassignment.adapter;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ualr.recyclerviewassignment.R;
+import com.ualr.recyclerviewassignment.model.Inbox;
+
+import java.util.List;
+
+public class AdapterList extends RecyclerView.Adapter {
+
+//  Created a new Adapter class and the corresponding ViewHolder class in a different file. The adapter will be used to populate
+//  the recyclerView and manage the interaction with the items in the list
+
+    private List<Inbox> mItems;
+
+    public AdapterList(List<Inbox> items) {
+        this.mItems = items;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater lyInflater = LayoutInflater.from(parent.getContext());
+        View itemView = lyInflater.inflate(R.layout.inbox_single_line_layout, parent);
+        RecyclerView.ViewHolder vh = new InboxViewHolder(itemView);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        InboxViewHolder inboxViewHolder = (InboxViewHolder) holder;
+        Inbox i = mItems.get(position);
+
+        inboxViewHolder.name.setText(i.getFrom());
+        inboxViewHolder.address.setText(i.getEmail());
+        inboxViewHolder.date.setText(i.getDate());
+        inboxViewHolder.message.setText(i.getMessage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.mItems.size();
+    }
+
+    private class InboxViewHolder extends RecyclerView.ViewHolder {
+        public ImageView image;
+        public TextView name;
+        public TextView address;
+        public TextView date;
+        public TextView letter;
+        public TextView message;
+
+        public InboxViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.thumbnail_image);
+            name = itemView.findViewById(R.id.sender_name);
+            address = itemView.findViewById(R.id.sender_address);
+            date = itemView.findViewById(R.id.the_date);
+            letter = itemView.findViewById(R.id.thumbnail_letter);
+            message = itemView.findViewById(R.id.message_body);
+        }
+    }
+
+}
